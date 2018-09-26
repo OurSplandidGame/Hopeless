9ad516a2d3f76a5c9c7e1405de32c885bd62ff91
handsonicv4 <sentaoli@usc.edu>
2018-09-26 11:25:29 -0700

    clean

---
+++ Assets/Yurowm/Weapons/LasserGun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LasserGun : MonoBehaviour {
+    public GameObject plasma;
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
